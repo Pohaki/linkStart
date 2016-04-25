9f6299b84aae3b04582f68b8ae9bd4bcae89eac5
Pohaki <kevin.lachaise@gmail.com>
2016-04-25 17:53:21 +0200

    ajustement animation

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ $(document).ready(function(){
     var entities = [].slice.call(document.querySelectorAll(".c"));
     entities.forEach(function (el) { el.emit('start'); });
     console.log("je suis cliqué")
-    $('#Start').die('click');
+    $('#Start').remove();
   });
 });
