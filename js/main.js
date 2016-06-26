f127b7ab8f8887a40acaef76e77f9ae9593b489a
Pohaki <kevin.lachaise@gmail.com>
2016-06-26 19:36:02 +0200

    add redirection

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     var entities = [].slice.call(document.querySelectorAll(".c"));
     entities.forEach(function (el) { el.emit('start'); });
     console.log("je suis cliqué")
-    $('#Start').die();
+    var link = 'window.location.replace("http://pohaki.github.io/TheWorldVR");';
+    setTimeout(function(){window.location.replace("http://kevinlachaise.fr")}, 5000);
   });
 });
